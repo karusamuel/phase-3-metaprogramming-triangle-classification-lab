2e2dca9b389de727d09d61bf23c64282ea72c56f
Karu Samuel <karusamuelngugi@gmail.com>
2022-09-07 14:25:03 +0300

    complete assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
   # write code here
+  @one
+  @two
+  @three
+  def initialize(one,two,three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+
+    if @one<=0 || @two <=0 || @three <=0 || (@one+@two)<=@three || (@one+@three)<=@two || (@two+@three)<=@one
+
+      raise TriangleError
+
+    else
+
+
+      if @one==@two && @two == @three
+        return :equilateral
+      elsif @one==@two || @two == @three || @one == @three
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+
+  end
+  class TriangleError < StandardError
+  
+  def message 
+    "you must give the get_married"
+  end
+  
+end
+
 end
